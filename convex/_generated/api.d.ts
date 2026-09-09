31b0c99cfddf9ee5578d2309f6db9a7bed7244fb
Payam <pchamani@nizek.com>
2026-09-09 14:44:27 +0330

    fix

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as crons from "../crons.js";
 import type * as jobs from "../jobs.js";
 import type * as lib_workerAuth from "../lib/workerAuth.js";
 
@@ -18,6 +19,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  crons: typeof crons;
   jobs: typeof jobs;
   "lib/workerAuth": typeof lib_workerAuth;
 }>;
